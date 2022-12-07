5d09b59f1e6bcd4310cfa2efdb5307d2748b3d54
Nicolas Pivaut - Technicien Informatique N1 - Lille <114007794+npivaut@users.noreply.github.com>
2022-12-07 18:20:32 +0100

    Update

--- SCRIPTAJOUTAD.ps1
+++ SCRIPTAJOUTAD.ps1
@@ -9,18 +9,13 @@
 
 
 
-# Import Active Directory module
 Import-Module ActiveDirectory
-# Formulaire Windows
 [System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms") | Out-Null
-# Créer et afficher la boîte de dialogue d'ouverture de fichier
 $dialog = New-Object System.Windows.Forms.OpenFileDialog
 $dialog.InitialDirectory = $StartDir
 $dialog.Filter = "CSV (*.csv)| *.csv" 
 $dialog.ShowDialog() | Out-Null
-# Chemin "Path"
 $CSVFile = $dialog.FileName
-# Import variables
 if ([System.IO.File]::Exists($CSVFile)) {
     Write-Host "Importing CSV..."
     $CSV = Import-Csv -LiteralPath "$CSVFile"
@@ -37,14 +32,11 @@ foreach($user in $CSV) {
    $EtatService = Get-Service -Name $NomService -ErrorAction Stop
    Write-Host -ForegroundColor Green "Etat du service correctement récupéré !"
 
-    # Mot de passe
     $SecurePassword = ConvertTo-SecureString "$($user.'First Name'[0])$($user.'Last Name')$($user.'Employee ID')!@#" -AsPlainText -Force
 
-    # Format de l'username
     $Username = "$($user.'First Name').$($user.'Last Name')"
     $Username = $Username.Replace(" ", "")
 
-    # Creation des utilisateurs
 
     New-ADUser -Name "$($user.'First Name') $($user.'Last Name')" `
                 -GivenName $user.'First Name' `
@@ -73,28 +65,7 @@ finally
 
 Write-Host "Contenu des erreurs : $Error"
 
-      #Partage dossier
-    $share = "E:\partage\$Username"  
-    $perso = "\\SERVAD\partage\$Username"
-    # Home de l'utilisateur dans le profil AD
-       New-Item -ItemType Directory "$share"
-     New-SmbShare -Name "$Username" -Path "$share" -ChangeAccess "$Username"
-
-     Set-ADUser $Username -HomeDrive "Z:" -HomeDirectory "$perso"
-     # Desactiver l'heritage
-     Get-Item "E:\partage\$Username" | Disable-NTFSAccessInheritance
-     # Modification proprietes du dossier
-     Set-NTFSOwner -Path "E:\partage\$Username" -Account "$Username@axeplane.loc"
-     # Suppression des droits d'accès au dossier perso
-     Remove-NTFSAccess -Path "E:\partage\$Username" -Account "Utilisateurs" -AccessRights FullControl
-     # Gestion des droits d'accès au dossier perso
-     Add-NTFSAccess -path "$share" -Account "$Username" -AccessRights Modify 
-   
- 
-
-
-
-
+     
     # If groups is not null... groupe de securite
     if ($User.'Add Groups (csv)' -ne "") {
         $User.'Add Groups (csv)'.Split(",") | ForEach {
@@ -103,7 +74,6 @@ Write-Host "Contenu des erreurs : $Error"
         }
     }
 
-    # resultat creation de l'utilisateur ou des utilisateurs
     Write-Host "Created user $Username with groups $($User.'Add Groups (csv)')"
 }
 Read-Host -Prompt "Script complete... Press enter to exit."
